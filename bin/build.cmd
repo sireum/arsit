71d9dd69bdf8cd6cba9a21e3ec98d971abbd2a37
Jason Belt <belt@ksu.edu>
2020-08-10 15:33:35 -0500

    update build.cmd

--- bin/build.cmd
+++ bin/build.cmd
@@ -137,6 +137,7 @@ def test(): Unit = {
 
 
 def clean(): Unit = {
+  println(s"Cleaning ${home}")
   val dirsToScrub: ISZ[Os.Path] = ISZ("air", "common", "hamr_codegen", "lib", "out", "runtime").map(m => home / m)
   dirsToScrub.foreach((m: Os.Path) => {
     println(s"Deleting ${m}")
