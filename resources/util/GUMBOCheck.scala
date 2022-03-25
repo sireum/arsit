492fa11742d25f3e96773036bd11403dcae2a599
John Hatcliff <hatcliff@ksu.edu>
2022-03-25 15:26:00 -0500

    Add initial support for ScalaCheck

---
+++ resources/util/GUMBOCheck.scala
@@ -0,0 +1,10 @@
+package __BASE_PACKAGE_NAME__
+
+import org.sireum.F32
+
+object GUMBOCheck {
+  // base type configuration
+  val F32_lower : F32 = -1000.0F
+  val F32_upper : F32 = 1000.0F
+  // trait Profile
+}
