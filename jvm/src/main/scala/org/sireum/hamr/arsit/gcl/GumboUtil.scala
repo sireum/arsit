31f8926fbbe8413295b78ee6930b51545d8a1a86
Jason Belt <belt@ksu.edu>
2022-11-29 15:07:53 -0600

    use interpolator lookup map

--- jvm/src/main/scala/org/sireum/hamr/arsit/gcl/GumboUtil.scala
+++ jvm/src/main/scala/org/sireum/hamr/arsit/gcl/GumboUtil.scala
@@ -3,6 +3,7 @@ package org.sireum.hamr.arsit.gcl
 
 import org.sireum._
 import org.sireum.lang.{ast => AST}
+import org.sireum.hamr.codegen.common.util.GclUtil
 
 object GumboUtil {
 
@@ -10,15 +11,8 @@ object GumboUtil {
     var imports: ISZ[ST] = ISZ()
 
     override def post_langastExpStringInterpolate(o: AST.Exp.StringInterpolate): MOption[AST.Exp] = {
-      o.prefix match {
-        case "s8" => imports = imports :+ st"org.sireum.S8._"
-        case "s16" => imports = imports :+ st"org.sireum.S16._"
-        case "s32" => imports = imports :+ st"org.sireum.S32._"
-        case "s64" => imports = imports :+ st"org.sireum.S64._"
-        case "u8" => imports = imports :+ st"org.sireum.U8._"
-        case "u16" => imports = imports :+ st"org.sireum.U16._"
-        case "u32" => imports = imports :+ st"org.sireum.U32._"
-        case "u64" => imports = imports :+ st"org.sireum.U64._"
+      GclUtil.interpolatorLookup.get(o.prefix) match {
+        case Some(e) => imports = imports :+ st"$e"
         case _ =>
       }
       return MNone()
