d1ad9700d31c3954a3e6a3fd8189f6d0f65b60c4
Jason Belt <belt@ksu.edu>
2023-08-22 16:47:24 -0500

    add runtime monitor interface and gui

--- jvm/src/main/scala/org/sireum/hamr/arsit/gcl/GumboXPlugin.scala
+++ jvm/src/main/scala/org/sireum/hamr/arsit/gcl/GumboXPlugin.scala
@@ -118,12 +118,12 @@ import org.sireum.message.Reporter
 
     var resources: ISZ[FileResource] = ISZ()
 
-    val runtimepackage = s"${arsitOptions.packageName}.runtimemonitor"
+    val runtimePackage = s"${arsitOptions.packageName}.runtimemonitor"
 
     val observationKind =
       st"""// #Sireum
           |
-          |package $runtimepackage
+          |package $runtimePackage
           |
           |import org.sireum._
           |import ${arsitOptions.packageName}._
@@ -139,7 +139,7 @@ import org.sireum.message.Reporter
     val process =
       st"""// #Sireum
           |
-          |package ${runtimepackage}
+          |package $runtimePackage
           |
           |import org.sireum._
           |import ${arsitOptions.packageName}._
@@ -149,7 +149,7 @@ import org.sireum.message.Reporter
           |object GumboXDispatcher {
           |  def dispatch(observationKind: ObservationKind.Type, preContainer: Option[art.DataContent], postContainer: Option[art.DataContent]): B = {
           |    observationKind match {
-          |      ${(this.entryPointHandlers, "\n")}
+          |      ${(entryPointHandlers, "\n")}
           |      case _ => halt("Infeasible")
           |    }
           |  }
@@ -157,9 +157,152 @@ import org.sireum.message.Reporter
 
     resources = resources :+ ResourceUtil.createResourceH(s"${runtimePath}/GumboXDispatcher.scala", process, T, F)
 
-    val blocks = ISZ(st"${runtimepackage}.RuntimeMonitor.init()")
+    val platformSetupBlocks = ISZ(st"${runtimePackage}.RuntimeMonitor.init()")
 
-    return ISZ(PlatformProviderPlugin.PlatformSetupContributions(imports = ISZ(), blocks = blocks, resources = resources))
+    val runtimeMonitor: ST =
+      st"""// #Sireum
+          |
+          |package $runtimePackage
+          |
+          |import org.sireum._
+          |import art.Art.BridgeId
+          |
+          |@ext object RuntimeMonitor {
+          |
+          |  def init(): Unit = $$
+          |
+          |  def observePreState(bridgeId: BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent]): Unit = $$
+          |
+          |  def observePostState(bridgeId: BridgeId, observationKind: ObservationKind.Type, post: art.DataContent): Unit = $$
+          |
+          |  def observePrePostState(bridgeId: BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent], post: art.DataContent): Unit = $$
+          |}
+          |"""
+    val rmpath = s"${runtimePath}/RuntimeMonitor.scala"
+    resources = resources :+ ResourceUtil.createResource(rmpath, runtimeMonitor, T)
+
+    val runtimeMonitorExt: ST =
+      st"""package $runtimePackage
+          |
+          |import org.sireum._
+          |import art.Art._
+          |
+          |object RuntimeMonitor_Ext {
+          |
+          |  var gui: GUI = _
+          |
+          |  def init(): Unit = {
+          |    gui = new GUI()
+          |    gui.init()
+          |  }
+          |
+          |  def observePreState(bridgeId: BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent]): Unit = {
+          |    gui.observePreState(bridgeId, observationKind, pre)
+          |  }
+          |
+          |  def observePostState(bridgeId: BridgeId, observationKind: ObservationKind.Type, post: art.DataContent): Unit = {
+          |    gui.observePostState(bridgeId, observationKind, post)
+          |  }
+          |
+          |  def observePrePostState(bridgeId: BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent], post: art.DataContent): Unit = {
+          |    gui.observePrePostState(bridgeId, observationKind, pre, post)
+          |  }
+          |}"""
+    val rmpathExt = s"${runtimePath}/RuntimeMonitor_Ext.scala"
+    resources = resources :+ ResourceUtil.createResource(rmpathExt, runtimeMonitorExt, T)
+
+    val gui: ST =
+      st"""package $runtimePackage
+          |
+          |import org.sireum._
+          |import art.Art.BridgeId
+          |import ${arsitOptions.packageName}.JSON
+          |
+          |import java.awt.{BorderLayout, Dimension}
+          |import javax.swing._
+          |import javax.swing.table.AbstractTableModel
+          |
+          |class GUI extends JFrame {
+          |
+          |  var jtable: JTable = _
+          |  var model: TableModel = _
+          |
+          |  def init(): Unit = {
+          |    this.setTitle("Visualizer")
+          |
+          |    model = new TableModel()
+          |    jtable = new JTable()
+          |    jtable.setModel(model)
+          |
+          |    add(jtable.getTableHeader(), BorderLayout.PAGE_START)
+          |    add(jtable, BorderLayout.CENTER)
+          |
+          |    setPreferredSize(new Dimension(500, 300))
+          |    pack()
+          |    setResizable(true)
+          |    setLocationRelativeTo(null)
+          |    setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
+          |    setVisible(true)
+          |  }
+          |
+          |  def observePreState(bridge: art.Art.BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent]): Unit = {
+          |    SwingUtilities.invokeLater(() => dispatch(bridge, observationKind, pre, None()))
+          |  }
+          |
+          |  def observePostState(bridge: art.Art.BridgeId, observationKind: ObservationKind.Type, post: art.DataContent): Unit = {
+          |    SwingUtilities.invokeLater(() => dispatch(bridge, observationKind, None(), Some(post)))
+          |  }
+          |
+          |  def observePrePostState(bridge: art.Art.BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent], post: art.DataContent): Unit = {
+          |    SwingUtilities.invokeLater(() => dispatch(bridge, observationKind, pre, Some(post)))
+          |  }
+          |
+          |  def dispatch(bridge: art.Art.BridgeId, observationKind: ObservationKind.Type, pre: Option[art.DataContent], post: Option[art.DataContent]): Unit = {
+          |    model.addRow(Row(bridge, observationKind,
+          |      GumboXDispatcher.dispatch(observationKind, pre, post),
+          |      if (pre.nonEmpty) Some(JSON.from_artDataContent(pre.get, T)) else None(),
+          |      if (post.nonEmpty) Some(JSON.from_artDataContent(post.get, T)) else None()))
+          |  }
+          |}
+          |
+          |case class Row(bridgeId: BridgeId, observationKind: ObservationKind.Type, result: Boolean, pre: Option[String], post: Option[String])
+          |
+          |class TableModel extends AbstractTableModel {
+          |  val columnNames = Array("BridgeId", "Kind", "Satisified")
+          |
+          |  var data: ISZ[Row] = ISZ()
+          |
+          |  def addRow(row: Row): Unit = {
+          |    data = data :+ row
+          |    fireTableRowsInserted(data.size.toInt - 1, data.size.toInt - 1)
+          |  }
+          |
+          |  override def getRowCount: Int = {
+          |    return data.size.toInt
+          |  }
+          |
+          |  override def getColumnCount: Int = {
+          |    return columnNames.length
+          |  }
+          |
+          |  override def getColumnName(column: Int): java.lang.String = {
+          |    return columnNames(column)
+          |  }
+          |
+          |  override def getValueAt(rowIndex: Int, columnIndex: Int): Object = {
+          |    return columnIndex match {
+          |      case 0 => data(rowIndex).bridgeId.string.native
+          |      case 1 => data(rowIndex).observationKind.string.native
+          |      case 2 => data(rowIndex).result.string.native
+          |      case _ => halt("Infeasible")
+          |    }
+          |  }
+          |}
+          |"""
+    val guipath = s"${runtimePath}/GUI.scala"
+    resources = resources :+ ResourceUtil.createResource(guipath, gui, T)
+
+    return ISZ(PlatformProviderPlugin.PlatformSetupContributions(imports = ISZ(), blocks = platformSetupBlocks, resources = resources))
   }
 
   /******************************************************************************************
@@ -336,7 +479,7 @@ import org.sireum.message.Reporter
           |      ${containers.observePostState_wL()}
           |
           |    // the rest can now be performed via a different thread
-          |    ${runtimePackage}.RuntimeMonitor.update1(${componentNames.archInstanceName}.id, $postInitKind, $postContainer)
+          |    ${runtimePackage}.RuntimeMonitor.observePostState(${componentNames.archInstanceName}.id, $postInitKind, $postContainer)
           |  }
           |
           |  def ${preComputeMethodName}(): Unit = {
@@ -345,7 +488,7 @@ import org.sireum.message.Reporter
           |      ${containers.observePreState_wL()})
           |
           |    // the rest can now be performed via a different thread
-          |    ${runtimePackage}.RuntimeMonitor.update1(${componentNames.archInstanceName}.id, $preComputeKind, ${preContainer}.get)
+          |    ${runtimePackage}.RuntimeMonitor.observePreState(${componentNames.archInstanceName}.id, $preComputeKind, ${preContainer}.asInstanceOf[Option[art.DataContent]])
           |  }
           |
           |  def ${postComputeMethodName}(): Unit = {
@@ -354,19 +497,18 @@ import org.sireum.message.Reporter
           |      ${containers.observePostState_wL()}
           |
           |    // the rest can now be performed via a different thread
-          |    ${runtimePackage}.RuntimeMonitor.update2(${componentNames.archInstanceName}.id, $postComputeKind, ${preContainer}.get, $postContainer)
+          |    ${runtimePackage}.RuntimeMonitor.observePrePostState(${componentNames.archInstanceName}.id, $postComputeKind, ${preContainer}.asInstanceOf[Option[art.DataContent]], $postContainer)
           |  }
           |}"""
 
     val path = s"${projectDirectories.bridgeDir}/${componentNames.packagePath}/${epCompanionName}.scala"
-    val epCompanionExtR =
-      ResourceUtil.createResource(path, epCompanionExt, T)
+    resources = resources :+ ResourceUtil.createResource(path, epCompanionExt, T)
 
     return EntryPointProviderPlugin.EntryPointContributions(
       imports = ISZ(),
       bridgeCompanionBlocks = ISZ(),
       entryPoint = gEntryPoint,
-      resources = resources :+ epCompanionExtR
+      resources = resources
     )
   }
 
