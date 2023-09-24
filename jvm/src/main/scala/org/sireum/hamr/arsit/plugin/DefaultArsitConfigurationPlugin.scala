8568b2325b2717c5230b8c94725d57543bc060fc
Jason Belt <belt@ksu.edu>
2023-09-24 17:31:34 -0500

    tipe fix

--- jvm/src/main/scala/org/sireum/hamr/arsit/plugin/DefaultArsitConfigurationPlugin.scala
+++ jvm/src/main/scala/org/sireum/hamr/arsit/plugin/DefaultArsitConfigurationPlugin.scala
@@ -4,5 +4,7 @@ package org.sireum.hamr.arsit.plugin
 import org.sireum._
 
 @record class DefaultArsitConfigurationPlugin extends ArsitConfigurationPlugin {
-  override def name: String = "DefaultArsitConfigurationPlugin"
+  override def name: String = {
+    return "DefaultArsitConfigurationPlugin"
+  }
 }
